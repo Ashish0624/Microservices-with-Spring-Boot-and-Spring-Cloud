285ffb799e98b1c8fdfc4399e512cff4dbfb0b51
Ashish0624 <65132166+Ashish0624@users.noreply.github.com>
2022-06-10 14:14:45 +0530

    First Commit

---
+++ Master-Microservices-with-Spring-Boot-and-Spring-Cloud/restful-web-services/src/main/java/com/ashish/rest/webservices/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}

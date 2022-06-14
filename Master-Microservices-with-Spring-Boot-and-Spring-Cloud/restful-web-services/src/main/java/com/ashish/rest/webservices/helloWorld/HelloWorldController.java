bac535b241a2fe35a48286ad434d5fc4d590c402
Ashish <ashishbaranwal361@gmail.com>
2022-06-14 17:00:41 +0530

    Swagger Implementation

--- Master-Microservices-with-Spring-Boot-and-Spring-Cloud/restful-web-services/src/main/java/com/ashish/rest/webservices/helloWorld/HelloWorldController.java
+++ Master-Microservices-with-Spring-Boot-and-Spring-Cloud/restful-web-services/src/main/java/com/ashish/rest/webservices/helloWorld/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.ashish.rest.webservices.helloWorld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping("/hello")
 	public String helloWorld() {
@@ -21,5 +28,10 @@ public class HelloWorldController {
 	@GetMapping("/hello/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World!, %s ", name));
+	}
+	
+	@GetMapping("/hello-internationalization")
+	public String helloWorldiInt(@RequestHeader(name="Accept-Language", required = false)Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 }
--- Master-Microservices-with-Spring-Boot-and-Spring-Cloud/restful-web-services/src/main/java/com/ashish/rest/webservices/user/User.java
+++ Master-Microservices-with-Spring-Boot-and-Spring-Cloud/restful-web-services/src/main/java/com/ashish/rest/webservices/user/User.java
@@ -5,11 +5,14 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class User {
 
 	private Integer id;
 	@Size(min = 2)
 	private String name;
+	@ApiModelProperty("Birth dates should be in past")
 	@Past
 	private Date bdate;
 	
